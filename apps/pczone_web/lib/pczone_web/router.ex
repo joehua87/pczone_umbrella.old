b01a093bf9447b4b833d618dd116ea570adae92d
joehua87 <joehua87@gmail.com>
2022-07-17 09:02:31 +0700

    clean up browser

--- apps/pczone_web/lib/pczone_web/router.ex
+++ apps/pczone_web/lib/pczone_web/router.ex
@@ -1,18 +1,6 @@
 defmodule PczoneWeb.Router do
   use PczoneWeb, :router
 
-  import PczoneWeb.UserAuth
-
-  pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_live_flash
-    plug :put_root_layout, {PczoneWeb.LayoutView, :root}
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
-    plug :fetch_current_user
-  end
-
   pipeline :api do
     plug :accepts, ["json"]
 
@@ -36,8 +24,9 @@ defmodule PczoneWeb.Router do
     import Phoenix.LiveDashboard.Router
 
     scope "/" do
-      pipe_through :browser
-      live_dashboard "/dashboard", metrics: PczoneWeb.Telemetry
+      pipe_through [:fetch_session, :protect_from_forgery]
+
+      live_dashboard "/dashboard", metrics: SupersoiWeb.Telemetry
     end
   end
 
@@ -47,41 +36,10 @@ defmodule PczoneWeb.Router do
   # node running the Phoenix server.
   if Mix.env() == :dev do
     scope "/dev" do
-      pipe_through :browser
+      pipe_through [:fetch_session, :protect_from_forgery]
 
       forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
-  end
-
-  ## Authentication routes
-
-  scope "/", PczoneWeb do
-    pipe_through [:browser, :redirect_if_user_is_authenticated]
-
-    get "/user/register", UserRegistrationController, :new
-    post "/user/register", UserRegistrationController, :create
-    get "/user/log_in", UserSessionController, :new
-    post "/user/log_in", UserSessionController, :create
-    get "/user/reset_password", UserResetPasswordController, :new
-    post "/user/reset_password", UserResetPasswordController, :create
-    get "/user/reset_password/:token", UserResetPasswordController, :edit
-    put "/user/reset_password/:token", UserResetPasswordController, :update
-  end
-
-  scope "/", PczoneWeb do
-    get "/user/settings", UserSettingsController, :edit
-    put "/user/settings", UserSettingsController, :update
-    get "/user/settings/confirm_email/:token", UserSettingsController, :confirm_email
-  end
-
-  scope "/", PczoneWeb do
-    pipe_through [:browser]
-
-    delete "/user/log_out", UserSessionController, :delete
-    get "/user/confirm", UserConfirmationController, :new
-    post "/user/confirm", UserConfirmationController, :create
-    get "/user/confirm/:token", UserConfirmationController, :edit
-    post "/user/confirm/:token", UserConfirmationController, :update
   end
 
   scope "/", PczoneWeb do
