115faf03c4f9280656592740b9bc1d430f22a0be
joehua87 <joehua87@gmail.com>
2022-09-24 13:49:25 +0700

    clean up

--- config/config.exs
+++ config/config.exs
@@ -49,10 +49,6 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
-config :nanoid,
-  size: 21,
-  alphabet: "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
