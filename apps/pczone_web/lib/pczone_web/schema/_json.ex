f48f60c0e3cefb248cce15786ebcc8e75daa9eca
joehua87 <joehua87@gmail.com>
2022-11-30 15:27:48 +0700

    json type as object instead of string

--- apps/pczone_web/lib/pczone_web/schema/_json.ex
+++ apps/pczone_web/lib/pczone_web/schema/_json.ex
@@ -5,33 +5,8 @@ defmodule PczoneWeb.Schema.Types.JSON do
   """
   use Absinthe.Schema.Notation
 
-  scalar :json, name: "Json" do
-    description("""
-    The `Json` scalar type represents arbitrary json string data, represented as UTF-8
-    character sequences. The Json type is most often used to represent a free-form
-    human-readable json string.
-    """)
-
-    serialize(&encode/1)
-    parse(&decode/1)
+  scalar :json, open_ended: true do
+    parse fn value -> {:ok, value} end
+    serialize fn value -> value end
   end
-
-  @spec decode(Absinthe.Blueprint.Input.String.t()) :: {:ok, term()} | :error
-  @spec decode(Absinthe.Blueprint.Input.Null.t()) :: {:ok, nil}
-  defp decode(%Absinthe.Blueprint.Input.String{value: value}) do
-    case Jason.decode(value) do
-      {:ok, result} -> {:ok, result}
-      _ -> :error
-    end
-  end
-
-  defp decode(%Absinthe.Blueprint.Input.Null{}) do
-    {:ok, nil}
-  end
-
-  defp decode(_) do
-    :error
-  end
-
-  defp encode(value), do: value
 end
