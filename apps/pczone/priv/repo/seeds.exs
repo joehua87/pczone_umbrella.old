b1c34efb93d56a90c4c294060ea80da62d468694
joehua87 <joehua87@gmail.com>
2022-12-11 15:19:06 +0700

    add default admin

--- apps/pczone/priv/repo/seeds.exs
+++ apps/pczone/priv/repo/seeds.exs
@@ -9,3 +9,8 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+with {:ok, user} <-
+       Pczone.Users.register_user(%{email: "admin@pczone.vn", password: "fdsajkl;", role: :admin}) do
+  Pczone.Users.set_role(user, :admin)
+end
